52fe7203770ee25a4b27746f13d625558a4223ae
sheenashah <shah.sheena19@gmail.com>
2019-06-05 09:14:41 -0400

    minor changes

--- middleware/index.js
+++ middleware/index.js
@@ -21,7 +21,7 @@ middlewareObj.checkCampgroundOwnership = function (req, res, next) {
             }
         });
     } else {
-        req.flash("error", "You need to be logged in to dao that");
+        req.flash("error", "You need to be logged in to do that");
         // if not redirect
         res.redirect("back");
 
